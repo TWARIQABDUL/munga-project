9abfc19c691adbd21c95452d787054305a5df905
TWARIQABDUL <tariqazizibabu@gmail.com>
2025-06-16 10:11:17 +0300

    Add mobile menu functionality and login page
    - Introduced SVG icons for close and menu buttons.
    - Updated index.html to include a mobile menu with navigation links.
    - Implemented scrolling reminder feature in index.html.
    - Created login.html for user account creation with form validation.
    - Enhanced main.js to handle mobile menu display and close actions.
    - Added animated floating labels in a new test HTML file for registration.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 
-
-console.log(formGroup)
+ const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+        const closeMenuButton = document.getElementById('close');
+        console.log(closeMenuButton);
+        
+        menu.addEventListener('click', () => {
+            mobileMenuContainer.style.display = 'flex';
+        });
+        closeMenuButton.addEventListener('click', () => {
+            
+            mobileMenuContainer.style.display = 'none';
+        });
