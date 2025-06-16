e9824f6d3fb176a81e916231444abf737b2a815f
TWARIQABDUL <tariqazizibabu@gmail.com>
2025-06-16 10:18:29 +0300

    Refactor mobile menu event listeners and clean up login script comments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
- const mobileMenuContainer = document.querySelector('.mobile-menu-container');
-        const closeMenuButton = document.getElementById('close');
-        console.log(closeMenuButton);
-        
-        menu.addEventListener('click', () => {
-            mobileMenuContainer.style.display = 'flex';
-        });
-        closeMenuButton.addEventListener('click', () => {
-            
-            mobileMenuContainer.style.display = 'none';
-        });
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+const closeMenuButton = document.getElementById('close');
+console.log(closeMenuButton);
+
+menu.addEventListener('click', () => {
+    mobileMenuContainer.style.display = 'flex';
+});
+closeMenuButton.addEventListener('click', () => {
+
+    mobileMenuContainer.style.display = 'none';
+});
